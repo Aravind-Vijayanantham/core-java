6c97227bb75ca18afdb5c25eb6c09765d84456e3
Aravind-Vijayanantham <aravind.viji.1999@gmail.com>
2020-09-21 20:07:13 +0530

    Instructions added

--- quadratic/src/Quadratic_formula.java
+++ quadratic/src/Quadratic_formula.java
@@ -3,15 +3,19 @@ public class Quadratic_formula
 {
     public static void main(String[] args)
     {
+        System.out.println("To find quadratic values enter non zero numbers");
         Scanner scan = new Scanner (System.in);
+        System.out.println("Enter the value for a");
         double a = scan.nextDouble();
+        System.out.println("Enter the value for b");
         double b = scan.nextDouble();
+        System.out.println("Enter the value for c");
         double c = scan.nextDouble();
         double sqrt = Math.sqrt(b * b - 4 * a * c);
         double result_one = (-1 * b + sqrt) / (2*a);
         double result_two = (-1 * b - sqrt) / (2*a);
-        System.out.println(result_one);
-        System.out.println(result_two);
+        System.out.println("The first result is" +result_one);
+        System.out.println("The second result is" +result_two);
 
 
     }
