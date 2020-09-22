400d84dc45c4015627832ac596076d0d89f374e1
Aravind-Vijayanantham <aravind.viji.1999@gmail.com>
2020-09-22 18:08:12 +0530

    edited

--- area/src/Circle.java
+++ area/src/Circle.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Circle {
     public static void main(String[] args)
     {
+        System.out.println("Enter the radius");
         Scanner scan = new Scanner(System.in);
-        double radius = scan.nextDouble();
-        double area= Math.PI * radius*radius;
-        double circumference = 2* Math.PI * radius;
-        System.out.println("Area of circle is"+area+"\n"+"circumference of the circle is"+circumference + ".");
+        int radius = scan.nextInt();
+        int area= (int) (Math.PI * radius*radius);
+        int circumference = (int) (2* Math.PI * radius);
+        System.out.println("Area of circle is "+area+"\n"+"circumference of the circle is "+circumference + ".");
     }
 }
